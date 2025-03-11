bd74716b470ddea3095ceb1ee92804d7a2cf188c
Panita <panita.phe@ku.th>
2025-03-11 16:49:31 +0700

    ttt

--- login/php/logout.php
+++ login/php/logout.php
@@ -10,7 +10,14 @@ if (!isset($_SESSION['id'])) {
     exit();
 }
 
-session_unset();
+$_SESSION = [];
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 session_destroy();
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
